c4bb2013b61841c0383a3caa1e31ac61d236e7bb
Gabriel Maia <gribeiromaia736@gmail.com>
2022-12-06 22:49:51 -0300

    iniciando projeto

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Banner.css'
+
+const Banner = () => {
+  return (
+    <div>
+        <header className='banner'>
+        <img src="components/imagens/banner.png" alt='Banner principal'/>
+        </header>
+      
+    </div>
+  )
+}
+
+export default Banner
