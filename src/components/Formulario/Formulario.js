2fea4770d328d7912a942c1d92d42b4113a8d45e
Gabriel Maia <gribeiromaia736@gmail.com>
2022-12-08 12:55:57 -0300

    adicionando listas

--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import CampoTexto from '../CampoTexto/CampoTexto'
+import ListaSuspensa from '../ListaSuspensa/ListaSuspensa'
 import "./Formulario.css"
 
 const Formulario = () => {
+   
+   const times = [ //Esse Array vai ser usado como valor para a nossa PROPS Itenss
+    'Programação',
+    'Front-End',
+    'Data-Science',
+    'UX e Design',
+    'Mobile',
+    'Inovação e Gestão'
+   ]
     return (
         <div>
             <section className='form'>
@@ -11,6 +21,8 @@ const Formulario = () => {
                     <CampoTexto label="Nome" placeholder='Digite seu nome' /> {/**Para importar automatico use o Control + space em cima do componente */}
                     <CampoTexto label="Cargo" placeholder='Digite seu cargo' />
                     <CampoTexto label="Imagem" placeholder='Digite o indereço da imagem' />
+                    {/**Abaixo vai ser passado a Lista Suspensa com as Props */}
+                    <ListaSuspensa itens={times}/>
                 </form>
             </section>
         </div>
---
+++ src/components/ListaSuspensa/ListaSuspensa.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./ListaSuspensa.css"
+
+const ListaSuspensa = (props) => {
+    //Nós iremos ter como props = Itens e a label
+    return (
+    <div>
+      <label>{props.label}</label>
+      <select>{/**Quero retornar os valores com o MAP */} {/*NÃO ESQUECA DA CHAVE KEY */}
+        {props.itens.map(item => <option key={item}>{item}</option>)} {/*Aqui eu quero retornar os valores que serão passados como props */}
+      </select>{/*O MAP se utiliza para tudo que você deseja retornar em Props */}
+    </div>
+  )
+}
+
+export default ListaSuspensa
